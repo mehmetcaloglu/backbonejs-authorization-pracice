09af16aacc7479251dec137755c481a62c74a94b
Aziz Unsal <unsal.aziz@gmail.com>
2015-07-06 18:28:47 +0300

    Completed sample Backbone view.

--- scripts/boilerplate.js
+++ scripts/boilerplate.js
@@ -4,8 +4,31 @@ define([
     'backbone',
 ], function($, _, Backbone) {
     // Private members are here.
+    
+    var MyView = Backbone.View.extend( {
+     
+        var initialize: function(options) {
+        
+        },
 
-    // The returned values can be used by other modules.
-    return { };
+        events: {
+                
+        },
+
+        render: function() {
+                
+        },
+
+
+        var functTest : function() {
+        // do something here
+        }
+
+    
+    
+    } );
+
+   // The returned values can be used by other modules.
+    return MyView; 
 
 });
